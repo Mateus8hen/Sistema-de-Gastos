d26402b4e14c266ce217624393f7a5d571c5fb2b
Mateus Henrique <Mateus8hen@gmail.com>
2026-07-25 11:44:09 -0300

    criando primeiros DTOS

---
+++ Back-end/Sistema-de-Gastos.API/DTOs/MetaFinanceira/MetaFinanceiroCreateDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gastos.API.DTOs.MetaFinanceira
+{
+    public class MetaFinanceiroCreateDto
+    {
+        public string Nome { get; set; } = string.Empty;
+
+        public decimal ValorObjetivo { get; set; }
+        
+
+        public int UsuarioId { get; set; }
+
+    }
+}
